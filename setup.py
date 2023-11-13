6e9781f8f39726a398d306ec44a472cc1dd242a8
Gowtham178 <149958053+Gowtham178@users.noreply.github.com>
2023-11-13 21:48:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     version='0.0.1',
     description='Gowtham178 python module',
     url='https://github.com/limegurutech/examples/tree/main/python/pip%20installable%20package',
-    author='Limeguru',
+    author='Gowtham178',
     author_email='contact@Gowtham178.com',
     packages=setuptools.find_packages(),
     install_requires=['pandas', 'pytest'
